61c27fb3fab33d673690c4b2148f4923e1502295
firdianr <100133856+firdianr@users.noreply.github.com>
2022-03-29 09:59:25 +0700

    Add files via upload

---
+++ Praktikum 6/Nomor 23 Program Tetris.cpp
@@ -0,0 +1,30 @@
+// Nama File    : Tetris.cpp
+// Deskripsi    : Menampilkan susunan karakter '*'.
+// Pembuat      : Firdian Rahmawan - 24060121120018
+// Tanggal Pembuatan : Selasa, 29 Maret 2022, 09.36 WIB
+
+#include<stdio.h>
+#include<iostream>
+
+int main()
+{
+    // Kamus
+    int i;
+    int j;
+    int n;
+
+    // Algoritma
+    printf("Masukkan Jumlah Baris = ");
+    scanf("%d", &n);
+
+    for (i=1 ; i<=n ; i++)
+    {
+        for (j=1 ; j<=i ; j++)
+        {
+            printf("* ");
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
---
+++ Praktikum 6/Nomor 25 Program Tetris2.cpp
@@ -0,0 +1,38 @@
+// Nama File    : Tetris2.cpp
+// Deskripsi    : Menampilkan susunan karakter '*'.
+// Pembuat      : Firdian Rahmawan - 24060121120018
+// Tanggal Pembuatan : Selasa, 29 Maret 2022, 09.53 WIB
+
+#include<stdio.h>
+#include<iostream>
+
+int main()
+{
+    // Kamus
+    int i;
+    int j;
+    int n;
+
+    // Algoritma
+    printf("Masukkan Jumlah Baris = ");
+    scanf("%d", &n);
+
+    for (i=1 ; i<=n ; i++)
+    {
+        for (j=1 ; j<=i ; j++)
+        {
+            printf("* ");
+        }
+        printf("\n");
+    }
+    for (i=n-1 ; i>=1 ; i--)
+    {
+        for (j=1 ; j<=i ; j++)
+        {
+            printf("* ");
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
